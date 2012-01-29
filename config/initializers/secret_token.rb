b3908b1eb631c54704a11dc7be10027dc6e4c8f7
Curren <curren.pangler+git@gmail.com>
2012-01-28 19:16:47 -0500

    Hello World

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kulfi::Application.config.secret_token = 'ee4383ee59e0d370d5e4f9dd29214a1be4a57d35f7176e3d5f426b5bd28f27f7850be1fab2ca7278b21118cf1231f8fb8185853ece2d4be2e6339fd248eace9a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Kulfi::Application.config.session_store :cookie_store, key: '_Kulfi_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Kulfi::Application.config.session_store :active_record_store
